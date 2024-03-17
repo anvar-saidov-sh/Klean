4d6bbebbd0bc31eefefd6541b801257a660f4e1d
anvarsaidovsh <bananoanimation12@gmail.com>
2024-03-17 14:16:45 +0500

    Working posts

--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -13,21 +13,6 @@ class PostSeeder extends Seeder
      */
     public function run()
     {
-        ModelsPost::create([
-            'user_id' => 1,
-            'title' => '$request->title',
-            'short_content' => '$request->short_content',
-            'content' => '$request->content',
-            'photo' => null,
-
-        ]);
-        ModelsPost::create([
-            'user_id' => 1,
-            'title' => '$request->title1',
-            'short_content' => '$request->short_content2',
-            'content' => '$request->content3',
-            'photo' => null,
-
-        ]);
+        
     }
 }
