e4c674b9af60a666bd07684a002b2aeceeeda02a
anvarsaidovsh <bananoanimation12@gmail.com>
2024-03-18 12:18:47 +0500

    Add condition

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,18 +1,20 @@
 <x-layouts.main>
     <x-slot:title>
-        Post - {{$post->id}}
+        Post - {{ $post->id }}
     </x-slot:title>
     <x-page-header>
-        Post - {{$post->id}}
+        Post - {{ $post->id }}
     </x-page-header>
-        <!-- Detail Start -->
+    <!-- Detail Start -->
     <div class="container-fluid py-5">
         <div class="container">
             <div class="row">
                 <div class="col-lg-8">
                     <div class="text-right">
-                        <a class="btn btn-sm btn-outline-success" href={{ route('posts.edit',['post' => $post->id])}}>Edit</a>
-                        <form action="{{route('posts.destroy', ['post' => $post->id])}}" method="POST" onsubmit="return confirm('Do you want to delete?')">
+                        <a class="btn btn-sm btn-outline-success"
+                            href={{ route('posts.edit', ['post' => $post->id]) }}>Edit</a>
+                        <form action="{{ route('posts.destroy', ['post' => $post->id]) }}" method="POST"
+                            onsubmit="return confirm('Do you want to delete?')">
                             @csrf
                             @method('DELETE')
                             <button class="btn btn-sm btn-outline-danger">
@@ -26,15 +28,19 @@
                             <span class="text-primary px-2">|</span>
                             <a class="text-secondary text-uppercase font-weight-medium" href="">Cleaning</a>
                             <span class="text-primary px-2">|</span>
-                            <a class="text-secondary text-uppercase font-weight-medium" href="">{{$post->created_at}}</a>
+                            <a class="text-secondary text-uppercase font-weight-medium"
+                                href="">{{ $post->created_at }}</a>
                         </div>
-                        <h1 class="section-title mb-3">{{$post->title}}</h1>
+                        <h1 class="section-title mb-3">{{ $post->title }}</h1>
                     </div>
 
                     <div class="mb-5">
-                        <img class="img-fluid rounded w-100 mb-4" src="{{asset('storage/'.$post->photo)}}" alt="Image">
-                        <p>{{$post->short_content}}</p>
-                        <p>{{$post->content}}</p>
+                        @if ($post->photo != null)
+                            <img class="img-fluid rounded w-100 mb-4" src="{{ asset('storage/' . $post->photo) }}"
+                                alt="Image">
+                        @endif
+                        <p>{{ $post->short_content }}</p>
+                        <p>{{ $post->content }}</p>
                     </div>
 
                     <div class="mb-5">
@@ -48,17 +54,22 @@
                             </div>
                         </div> --}}
                         <div class="media mb-4">
-                            <img src="/img/user.jpg" alt="Image" class="img-fluid rounded-circle mr-3 mt-1" style="width: 45px;">
+                            <img src="/img/user.jpg" alt="Image" class="img-fluid rounded-circle mr-3 mt-1"
+                                style="width: 45px;">
                             <div class="media-body">
                                 <h6>John Doe <small><i>01 Jan 2045 at 12:00pm</i></small></h6>
-                                <p>Diam amet duo labore stet elitr ea clita ipsum, tempor labore accusam ipsum et no at. Kasd diam tempor rebum magna dolores sed sed eirmod ipsum clita, at tempor amet ipsum diam tempor sit.</p>
+                                <p>Diam amet duo labore stet elitr ea clita ipsum, tempor labore accusam ipsum et no at.
+                                    Kasd diam tempor rebum magna dolores sed sed eirmod ipsum clita, at tempor amet
+                                    ipsum diam tempor sit.</p>
                                 <button class="btn btn-sm btn-light">Reply</button>
                                 <div class="media mt-4">
                                     <img src="/img/user.jpg" alt="Image" class="img-fluid rounded-circle mr-3 mt-1"
                                         style="width: 45px;">
                                     <div class="media-body">
                                         <h6>John Doe <small><i>01 Jan 2045 at 12:00pm</i></small></h6>
-                                        <p>Diam amet duo labore stet elitr ea clita ipsum, tempor labore accusam ipsum et no at. Kasd diam tempor rebum magna dolores sed sed eirmod ipsum clita, at tempor amet ipsum diam tempor sit.</p>
+                                        <p>Diam amet duo labore stet elitr ea clita ipsum, tempor labore accusam ipsum
+                                            et no at. Kasd diam tempor rebum magna dolores sed sed eirmod ipsum clita,
+                                            at tempor amet ipsum diam tempor sit.</p>
                                         <button class="btn btn-sm btn-light">Reply</button>
                                     </div>
                                 </div>
@@ -117,23 +128,28 @@
                         <h3 class="mb-4 section-title">Categories</h3>
                         <ul class="list-inline m-0">
                             <li class="mb-1 py-2 px-3 bg-light d-flex justify-content-between align-items-center">
-                                <a class="text-dark" href="#"><i class="fa fa-angle-right text-secondary mr-2"></i>Web Design</a>
+                                <a class="text-dark" href="#"><i
+                                        class="fa fa-angle-right text-secondary mr-2"></i>Web Design</a>
                                 <span class="badge badge-primary badge-pill">150</span>
                             </li>
                             <li class="mb-1 py-2 px-3 bg-light d-flex justify-content-between align-items-center">
-                                <a class="text-dark" href="#"><i class="fa fa-angle-right text-secondary mr-2"></i>Web Development</a>
+                                <a class="text-dark" href="#"><i
+                                        class="fa fa-angle-right text-secondary mr-2"></i>Web Development</a>
                                 <span class="badge badge-primary badge-pill">131</span>
                             </li>
                             <li class="mb-1 py-2 px-3 bg-light d-flex justify-content-between align-items-center">
-                                <a class="text-dark" href="#"><i class="fa fa-angle-right text-secondary mr-2"></i>Online Marketing</a>
+                                <a class="text-dark" href="#"><i
+                                        class="fa fa-angle-right text-secondary mr-2"></i>Online Marketing</a>
                                 <span class="badge badge-primary badge-pill">78</span>
                             </li>
                             <li class="mb-1 py-2 px-3 bg-light d-flex justify-content-between align-items-center">
-                                <a class="text-dark" href="#"><i class="fa fa-angle-right text-secondary mr-2"></i>Keyword Research</a>
+                                <a class="text-dark" href="#"><i
+                                        class="fa fa-angle-right text-secondary mr-2"></i>Keyword Research</a>
                                 <span class="badge badge-primary badge-pill">56</span>
                             </li>
                             <li class="py-2 px-3 bg-light d-flex justify-content-between align-items-center">
-                                <a class="text-dark" href="#"><i class="fa fa-angle-right text-secondary mr-2"></i>Email Marketing</a>
+                                <a class="text-dark" href="#"><i
+                                        class="fa fa-angle-right text-secondary mr-2"></i>Email Marketing</a>
                                 <span class="badge badge-primary badge-pill">98</span>
                             </li>
                         </ul>
@@ -144,17 +160,20 @@
                     <div class="mb-5">
                         <h3 class="mb-4 section-title">Recent Post</h3>
                         @foreach ($recent_posts as $post)
-                        <div class="d-flex align-items-center border-bottom mb-3 pb-3">
-                            <img class="/img-fluid rounded" src="/img/blog-1.jpg" style="width: 80px; height: 80px; object-fit: cover;" alt="">
-                            <div class="d-flex flex-column pl-3">
-                                <a class="text-dark mb-2" href="">{{$post->title}}</a>
-                                <div class="d-flex">
-                                    <small><a class="text-secondary text-uppercase font-weight-medium" href="">Admin</a></small>
-                                    <small class="text-primary px-2">|</small>
-                                    <small><a class="text-secondary text-uppercase font-weight-medium" href="">Cleaning</a></small>
+                            <div class="d-flex align-items-center border-bottom mb-3 pb-3">
+                                <img class="/img-fluid rounded" src="/img/blog-1.jpg"
+                                    style="width: 80px; height: 80px; object-fit: cover;" alt="">
+                                <div class="d-flex flex-column pl-3">
+                                    <a class="text-dark mb-2" href="">{{ $post->title }}</a>
+                                    <div class="d-flex">
+                                        <small><a class="text-secondary text-uppercase font-weight-medium"
+                                                href="">Admin</a></small>
+                                        <small class="text-primary px-2">|</small>
+                                        <small><a class="text-secondary text-uppercase font-weight-medium"
+                                                href="">Cleaning</a></small>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
                         @endforeach
                         {{-- <div class="d-flex align-items-center border-bottom mb-3 pb-3">
                             <img class="img-fluid rounded" src="/img/blog-2.jpg" style="width: 80px; height: 80px; object-fit: cover;" alt="">
@@ -229,5 +248,5 @@
             </div>
         </div>
     </div>
-        <!-- Detail End -->
+    <!-- Detail End -->
 </x-layouts.main>
