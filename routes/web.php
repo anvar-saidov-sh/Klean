84dcf1d7812148dd00ef4796f7472205b39cb477
anvarsaidovsh <bananoanimation12@gmail.com>
2024-03-18 21:49:53 +0500

    Update all

--- routes/web.php
+++ routes/web.php
@@ -20,8 +20,7 @@ Route::get('/contact', [PageController::class, 'contact'])->name('contact');
 Route::resource('posts', PostController::class);
 Route::resource('comments', CommentController::class);
 Route::resource('comments', CommentController::class);
-Route::resource('comments', CommentController::class);
-Route::resource('comments', CommentController::class);
+Route::resource('users', UserController::class);
 
 // Route::resource([
 //     'posts' => PostController::class,
