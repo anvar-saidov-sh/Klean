e25973f43658323f87aeb2c031faed780bb0524c
anvarsaidovsh <bananoanimation12@gmail.com>
2024-03-24 14:03:14 +0500

    Issues in leaving comments

---
+++ app/Models/Comment.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'body',
+        'post_id',
+        'user_id',
+    ];
+     
+    public function post()
+    {
+        $this->belongsTo(Post::class);
+    }
+    public function user()
+    {
+        $this->belongsTo(User::class);
+    }
+}
