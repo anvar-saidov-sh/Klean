1510dc56b5d706739124f7131a85c4de7b5f6f47
anvarsaidovsh <bananoanimation12@gmail.com>
2024-03-25 12:41:23 +0500

    Working on category part

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Category;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -15,6 +17,7 @@ class DatabaseSeeder extends Seeder
 
         $this->call([
             UserSeeder::class,
+            Category::class,
             PostSeeder::class,
         ]);
     }
