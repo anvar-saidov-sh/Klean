f4fe029ad81a1e99552f8ed22126a34e0cd82ed6
anvarsaidovsh <bananoanimation12@gmail.com>
2024-03-25 12:29:54 +0500

    Working on category part

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -19,7 +19,12 @@ class Post extends Model
     public function user(){
         return $this->belongsTo(User::class);
     }
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
     public function comments() {
         return $this->hasMany(Comment::class);
     }
+
+
 }
