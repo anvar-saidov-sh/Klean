101bbc0f61ed2c507328b614dadfc89689f3da36
anvarsaidovsh <bananoanimation12@gmail.com>
2024-03-25 12:53:48 +0500

    Working on category part

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -14,6 +14,7 @@ class CategorySeeder extends Seeder
     public function run(): void
     {
         Category::create(['name' => 'Web Design']);
+        Category::create(['name' => 'Web Development']);
         Category::create(['name' => 'Online Marketing']);
         Category::create(['name' => 'Keyword Research']);
         Category::create(['name' => 'Email Marketing']);
