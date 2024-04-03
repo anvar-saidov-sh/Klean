b915e1b99c44c8422f2d3ab6a134a3aba0bb5522
anvarsaidovsh <bananoanimation12@gmail.com>
2024-04-03 20:56:51 +0500

    Update

--- resources/views/components/layouts/main.blade.php
+++ resources/views/components/layouts/main.blade.php
@@ -156,7 +156,7 @@
 
 
     <!-- JavaScript Libraries -->
-    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src={{ asset('https://code.jquery.com/jquery-3.4.1.min.js')}}></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
     <script src="/lib/easing/easing.min.js"></script>
     <script src="/lib/waypoints/waypoints.min.js"></script>
@@ -170,7 +170,7 @@
     <script src="/mail/contact.js"></script> --}}
 
     <!-- Template Javascript -->
-    <script src="/js/main.js"></script>
+    <script src="./js/main.js"></script>
 </body>
 
 </html>
