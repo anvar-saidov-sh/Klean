32ab291f719ed4bccf25790db4496b12034742aa
anvarsaidovsh <bananoanimation12@gmail.com>
2024-04-06 14:42:29 +0500

    Review

--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -8,9 +8,6 @@ use Illuminate\Database\Seeder;
 
 class PostSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run()
     {
         Post::factory()->count(20)->create();
