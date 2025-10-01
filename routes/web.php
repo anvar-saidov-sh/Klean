1e67cd07af2c136376df1a51541a64c15de6add0
anvarbeck <anvarsaidov732@gmail.com>
2025-10-01 12:11:31 +0500

    Routing

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ Route::get('/', [PageController::class, 'main']);
 Route::get('/about', [PageController::class, 'about'])->name('about');
 Route::get('/service', [PageController::class, 'service'])->name('service');
 Route::get('/project', [PageController::class, 'project'])->name('project');
-// Route::get('/blog', [PageController::class, 'blog'])->name('blog');
+Route::get('/blog', [PageController::class, 'blog'])->name('blog');
 Route::get('/contact', [PageController::class, 'contact'])->name('contact');
 
 
